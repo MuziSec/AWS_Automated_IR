f80f765475bbb1dc18b6e1057a448519203f29ad
MuziSec <a.jared.anderson@gmail.com>
2020-09-01 09:14:19 -0400

    refactored a bunch of code

--- modules/ec2_contain_and_collect.py
+++ modules/ec2_contain_and_collect.py
@@ -13,6 +13,63 @@ def collect_metadata(ec2, instance_id):
         return response
     except Exception as e:
         return(f'Could not collect instance metadata. The following error occurred: {e}')
+def isolate_ec2(ec2, instance_id):
+    # TO DO: Switch VPC Security Group to isolate the instance
+    return
+
+def deregister_elb_service(instance_id):
+    # Get ELB Names
+    elb = boto3.client('elb')
+    try:
+        elb_response = elb.describe_load_balancers()
+    except Exception as e:
+        return(f'There was an issue describing the load balancers: {e}')
+    # Store all elbs for the instance in elb dict
+    elb_dict = {}
+    # Remove from ELB
+    for load_balancer in elb_response['LoadBalancerDescriptions']:
+        load_balancer_name = load_balancer['LoadBalancerName']
+        try:
+            deregister_response = elb.deregister_instances_from_load_balancer(
+                LoadBalancerName=load_balancer_name, 
+                Instances=[
+                    {
+                        'InstanceId': instance_id
+                    },
+                ]
+            )
+        except Exception as e:
+            return(f'There was an issue deregistering {instance_id} from ELB: {load_balancer_name}: {e}')
+        # Check if instance successfully deregistered from ELB
+        if instance_id not in [instance['InstanceId'] for instance in deregister_response['Instances']]:
+            elb_dict[load_balancer_name] = f'{instance_id} successfully deregistered from {load_balancer_name}'
+        else:
+            elb_dict[load_balancer_name] = f'{instance_id} failed to deregister from {load_balancer_name}'
+    return elb_dict
+    
+def detach_autoscaling(instance_id):
+    # Get autoscaling group names for instance
+    autoscaling = boto3.client('autoscaling')
+    try:
+        asg_response = autoscaling.describe_auto_scaling_instances(InstanceIds=[instance_id],)
+    except Exception as e:
+        return(f'There was an issue describing the instance: {e}')
+    # Store all asgs for the instance in asg dict
+    asg_dict = {}    
+    # Remove from asg
+    for asi in asg_response['AutoScalingInstances']:
+        asg = asi['AutoScalingGroupName']
+        try:
+            response = autoscaling.detach_instances(InstanceIds=[instance_id], AutoScalingGroupName=asg, ShouldDecrementDesiredCapacity=True)
+        except Exception as e:
+            return(f'There was an issue detaching {instance_id} from {asg}: {e}')
+        
+        # Check if successfully detached
+        if 'Detaching' in response['Activities'][0]['Description']:
+            asg_dict[asg] = response['Activities'][0]['Description']
+        else:
+            asg_dict[asg] = "Failed to detach from auto scaling group."
+    return asg_dict
 
 def enable_termination_protection(ec2, instance_id):
 
@@ -59,64 +116,6 @@ def get_volumes(ec2, instance_id):
             ir_volumes.append(volume)
     return ir_volumes
 
-def isolate_ec2(ec2, instance_id):
-    # TO DO: Switch VPC Security Group to isolate the instance
-    return
-
-def detach_autoscaling(instance_id):
-    # Get autoscaling group names for instance
-    autoscaling = boto3.client('autoscaling')
-    try:
-        asg_response = autoscaling.describe_auto_scaling_instances(InstanceIds=[instance_id],)
-    except Exception as e:
-        return(f'There was an issue describing the instance: {e}')
-    # Store all asgs for the instance in asg dict
-    asg_dict = {}    
-    # Remove from asg
-    for asi in asg_response['AutoScalingInstances']:
-        asg = asi['AutoScalingGroupName']
-        try:
-            response = autoscaling.detach_instances(InstanceIds=[instance_id], AutoScalingGroupName=asg, ShouldDecrementDesiredCapacity=True)
-        except Exception as e:
-            return(f'There was an issue detaching {instance_id} from {asg}: {e}')
-        
-        # Check if successfully detached
-        if 'Detaching' in response['Activities'][0]['Description']:
-            asg_dict[asg] = response['Activities'][0]['Description']
-        else:
-            asg_dict[asg] = "Failed to detach from auto scaling group."
-    return asg_dict
-
-def deregister_elb_service(instance_id):
-    # Get ELB Names
-    elb = boto3.client('elb')
-    try:
-        elb_response = elb.describe_load_balancers()
-    except Exception as e:
-        return(f'There was an issue describing the load balancers: {e}')
-    # Store all elbs for the instance in elb dict
-    elb_dict = {}
-    # Remove from ELB
-    for load_balancer in elb_response['LoadBalancerDescriptions']:
-        load_balancer_name = load_balancer['LoadBalancerName']
-        try:
-            deregister_response = elb.deregister_instances_from_load_balancer(
-                LoadBalancerName=load_balancer_name, 
-                Instances=[
-                    {
-                        'InstanceId': instance_id
-                    },
-                ]
-            )
-        except Exception as e:
-            return(f'There was an issue deregistering {instance_id} from ELB: {load_balancer_name}: {e}')
-        # Check if instance successfully deregistered from ELB
-        if instance_id not in [instance['InstanceId'] for instance in deregister_response['Instances']]:
-            elb_dict[load_balancer_name] = f'{instance_id} successfully deregistered from {load_balancer_name}'
-        else:
-            elb_dict[load_balancer_name] = f'{instance_id} failed to deregister from {load_balancer_name}'
-    return elb_dict
-
 def snapshot_ec2(ec2, instance_id):
     # Loop through volumes of interest and snapshot them
     ir_volumes = get_volumes(ec2, instance_id)
@@ -136,7 +135,8 @@ def snapshot_ec2(ec2, instance_id):
         
     return response_dict
     
-def capture_memory(instance_id, aws_region):
+    
+def run_command(instance_id, aws_region, commands):
 
     ssm_client = boto3.client('ssm', region_name=aws_region)
 
@@ -147,112 +147,102 @@ def capture_memory(instance_id, aws_region):
         DocumentName="AWS-RunShellScript",
         # TimeoutSeconds=1800,
         Parameters={
-            'commands':[
-                '/root/avml /root/memdump.lime'
-            ]
+            'commands':commands
         },
     )
     
+    # Get command_id and instance_id to check output
     command_id = response['Command']['CommandId']
     instance_id = response['Command']['InstanceIds'][0]
-    time.sleep(2)
-    response = ssm_client.get_command_invocation(CommandId=command_id, InstanceId=instance_id)
-    output = response['StandardOutputContent']
-    print(f'Response output is: {output}')
-
-def build_volatility_profile(instance_id, aws_region):
-    # Add profile to volatility/volatility/plugins/overlays/linux/ and then add x64 or x32 to name of zip (and exclude .zip)
-    ssm_client = boto3.client('ssm', region_name=aws_region)
     
-    cmd0 = f'sudo su ec2-user'
-    cmd1 = f'sudo yum install -y kernel-devel-$(uname -r)'
-    cmd2 = f'sudo yum install -y libdwarf-tools'
-    cmd3 = f'git clone https://github.com/volatilityfoundation/volatility.git /home/ec2-user/volatility'
-    cmd4 = f'sudo chown -R ec2-user /home/ec2-user/volatility/tools/linux'
-    cmd5 = f'cd /home/ec2-user/volatility/tools/linux/'
-    cmd6 = f'make'
-    cmd7 = f'cd /home/ec2-user/volatility'
-    cmd8 = f'zip /home/ec2-user/Linux_`uname -r`.zip tools/linux/module.dwarf /boot/System.map-`uname -r`'
-    cmd9 = f'cp /home/ec2-user/Linux_`uname -r`.zip volatility/plugins/overlays/linux/'
-    cmd10 = f'ls /home/ec2-user/*.zip'
-
-    response = ssm_client.send_command(
-        InstanceIds=[
-            instance_id
-        ],
-        DocumentName="AWS-RunShellScript",
-        # TimeoutSeconds=1800,
-        Parameters={
-            'commands':[
-                cmd1,
-                cmd2,
-                cmd3,
-                cmd4,
-                cmd5,
-                cmd6,
-                cmd7,
-                cmd8,
-                cmd9,
-                cmd10
-
-            ]
-        },
-    )
+    # Wait for command to complete
+    time.sleep(60)
     
-    cmdId = response.get("Command").get("CommandId")
+    try:
+        cmd_response = ssm_client.get_command_invocation(
+            CommandId=command_id,
+            InstanceId=instance_id
+        )
+    except Exception as e:
+        print(f'Could not get response: {e}')
 
-    time.sleep(30)
+        
+    return cmd_response
     
-    cmd_response = ssm_client.get_command_invocation(
-        CommandId=cmdId,
-        InstanceId=instance_id
-    )
-    print(f'Command Response: {cmd_response}')
+def capture_memory(instance_id, aws_region, commands):
+    ''' Capture Memory and Upload to S3. Returns S3 bucket key (~filepath)'''
     
-def get_s3_presigned(bucket_name, object_name, expiration=7200):
+    # Execute memdump and return path of memdump output
+    response = run_command(instance_id, aws_region, commands)
+    memdump_path = response['StandardOutputContent'].rstrip()
+    
+    # S3 Key
+    key = 'memdump ' + instance_id
+    
+    # Generate presigned_url for upload
+    presigned_url = get_s3_presigned('<s3_bucket_name>', key, 'put_object')
+    
+    # Upload memdump to S3
+    try:
+        upload_response = upload_files(instance_id, aws_region, presigned_url, memdump_path)
+    except Exception as e:
+        return False
+    
+    # Return memdump key in S3 bucket
+    return key
+    
+def build_volatility_profile(instance_id, aws_region, commands):
+    ''' Build Volatility Profile and upload to S3. Returns S3 bucket key (~filepath)'''
+    
+    # Grab requirements and build profile
+    response = run_command(instance_id, aws_region, commands)
+    
+    # Vol profile path
+    vol_profile_path = "/home/ec2-user/Linux_`uname -r`.zip"
+    
+    
+    # S3 Key
+    key = 'VolatilityProfile ' + instance_id
+    
+    # Generate presigned_url for upload
+    presigned_url = get_s3_presigned('<s3_bucket_name>', key, 'put_object')
+    
+    # Upload profile to S3
+    try:
+        upload_response = upload_files(instance_id, aws_region, presigned_url, vol_profile_path)
+    except Exception as e:
+        return False
+    
+    # Return profile key in S3 bucket
+    return key
+   
+    
+    
+def get_s3_presigned(bucket_name, object_name, method, expiration=7200):
     '''Generate a pre-signed URL to allow S3 upload of files'''
     s3 = boto3.client('s3')
     
     try:
-        response = s3.generate_presigned_url('get_object', Params={'Bucket': bucket_name, 'Key': object_name}, ExpiresIn=expiration, HttpMethod='PUT')
+        response = s3.generate_presigned_url(method, Params={'Bucket': bucket_name, 'Key': object_name}, ExpiresIn=expiration, HttpMethod='PUT')
     except ClientError as e:
         print(f'There was an error generating the presigned url: {e}')
         return None
     return response
 
-def get_files(instance_id, aws_region):
-    '''Get pre-signed URL upload memdump and volatility profile'''
-    memdump_url = get_s3_presigned('s3_bucket', 'memdump')
-    print(f'Memdump URL: {memdump_url}')
+def upload_files(instance_id, aws_region, presigned_url, filepath):
+    '''Upload files to S3 bucket'''
     
-    # open ssm client
-    ssm_client = boto3.client('ssm', region_name=aws_region)
+    # Create the command to execute upload
+    cmd_str = f"curl -v --upload-file {filepath} '{presigned_url}'"
+    #print(f'Command_String is: {cmd_str}')
+    commands = [cmd_str]
     
-    cmd0 = f'curl -v --upload-file /home/ec2-user/memdump.lime {memdump_url}'
+    # Execute the upload
+    response = run_command(instance_id, aws_region, commands)
     
-    response = ssm_client.send_command(
-        InstanceIds=[
-            instance_id
-        ],
-        DocumentName="AWS-RunShellScript",
-        # TimeoutSeconds=1800,
-        Parameters={
-            'commands':[
-                cmd0
-
-            ]
-        },
-    )
+    return response
+   
     
-    cmdId = response.get("Command").get("CommandId")
-    
-    time.sleep(30)
-    
-    cmd_response = ssm_client.get_command_invocation(
-        CommandId=cmdId,
-        InstanceId=instance_id
-    )
-    print(f'Command Response: {cmd_response}')
 
 def lambda_handler(event, context):
     
@@ -279,8 +269,8 @@ def lambda_handler(event, context):
     
     
     # Get EC2 instance metadata
-    # print(collect_metadata(ec2, instance_id))
-    '''
+    print(collect_metadata(ec2, instance_id))
+    
     # Prevent EC2 termination 
     
     term_protection = enable_termination_protection(ec2, instance_id)
@@ -302,14 +292,19 @@ def lambda_handler(event, context):
     
     #Deregister Load Balancers
     print(deregister_elb_service(instance_id))
-    '''
+    
     
     # Capture Memory
-    #capture_memory(instance_id, aws_region)
+    memdump_commands = ['/root/avml /root/memdump.lime','find /root/ -name memdump.lime']
+    s3_memdump_key = capture_memory(instance_id, aws_region, memdump_commands)
+    print(f'S3 Memdump Key: {s3_memdump_key}')
+    
     
     # Build Volatility Profile
-    # build_volatility_profile(instance_id, aws_region)
+    build_vol_profile_commands = ['sudo su ec2-user', 'sudo yum install -y kernel-devel-$(uname -r)', 'sudo yum install -y libdwarf-tools', 'git clone https://github.com/volatilityfoundation/volatility.git /home/ec2-user/volatility', 'sudo chown -R ec2-user /home/ec2-user/volatility/tools/linux', 'cd /home/ec2-user/volatility/tools/linux/', 'zip /home/ec2-user/Linux_`uname -r`.zip tools/linux/module.dwarf /boot/System.map-`uname -r`', 'find /home/ec2-user/ -name Linux_`uname -r`.zip']
+    s3_vol_profile_key = build_volatility_profile(instance_id, aws_region, build_vol_profile_commands)
+    print(f'S3 Vol Profile Key: {s3_vol_profile_key}')
     
-    # Upload Files to S3
-    get_files(instance_id, aws_region)
+
+    
 
